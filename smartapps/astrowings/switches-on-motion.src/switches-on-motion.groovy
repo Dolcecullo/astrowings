034df91914a95b8f71d477fcda61c36ed4108c20
Phil Maynard <astrowings@gmail.com>
2016-11-06 16:43:44 -0700

    v0.10 - initial coding for parent app

---
+++ smartapps/astrowings/switches-on-motion.src/switches-on-motion.groovy
@@ -0,0 +1,208 @@
+/**
+ *  Switches on Motion
+ *
+ *  Copyright © 2016 Phil Maynard
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                       */
+ 	       def urlApache() { return "http://www.apache.org/licenses/LICENSE-2.0" }      /*
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *	VERSION HISTORY                                    */
+ 	 def versionNum() {	return "version 0.10" }          /*
+ *
+ *    v0.10 (06-Nov-2016): initial coding for parent app
+ *
+*/
+definition(
+    name: "Switches on Motion",
+    namespace: "astrowings",
+    author: "Phil Maynard",
+    description: "Parent app for 'Switch on Motion'",
+    category: "Convenience",
+    iconUrl: "http://cdn.device-icons.smartthings.com/Home/home30-icn.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Home/home30-icn@2x.png",
+    iconX3Url: "http://cdn.device-icons.smartthings.com/Home/home30-icn@3x.png")
+
+
+//   ---------------------------
+//   ***   APP PREFERENCES   ***
+
+preferences {
+	page(name: "pageMain")
+    page(name: "pageSettings")
+    page(name: "pageUninstall")
+}
+
+
+//   --------------------------------
+//   ***   CONSTANTS DEFINITIONS  ***
+
+
+
+//   -----------------------------
+//   ***   PAGES DEFINITIONS   ***
+
+def pageMain() {
+	dynamicPage(name: "pageMain", title: "Main", install: true, uninstall: false) {
+    	section(){
+        	paragraph "", title: "Use this SmartApp to add child instances of the 'Switch on Motion' app."
+        }
+        section {
+            app name: "switchOnMotion", appName: "Switch on Motion", namespace: "astrowings", title: "Create New Automation", multiple: true
+        }
+		section() {
+            href "pageSettings", title: "App settings", image: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png", required: false
+		}
+    }
+}
+    
+def pageSettings() {
+	dynamicPage(name: "pageSettings", install: false, uninstall: false) {
+		section("About") {
+        	paragraph "Copyright ©2016 Phil Maynard\n${versionNum()}", title: app.name
+            href name: "hrefLicense", title: "License", description: "Apache License", url: urlApache()
+		}
+        /*
+        if (!theLuminance) { //TODO: apply this setting to all child instances?
+            section("This SmartApp uses the sunset/sunrise time to evaluate luminance as a criteria to trigger actions. " +
+                    "If required, you can adjust the amount time before/after sunset when the app considers that it's dark outside " +
+                    "(e.g. use '-20' to adjust the sunset time 20 minutes earlier than actual).") {
+                input "sunsetOffset", "number", title: "Sunset offset time", description: "How many minutes (+/- 60)?", range: "-60..60", required: false
+            }
+   		}
+        */
+   		section() {
+			label title: "Assign a name", defaultValue: "${app.name}", required: false
+            href "pageUninstall", title: "", description: "Uninstall this SmartApp", image: "https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/trash-circle-red-512.png", state: null, required: true
+		}
+        section("Debugging Options", hideable: true, hidden: true) { //TODO: apply this setting to all child instances?
+            input "debugging", "bool", title: "Enable debugging", defaultValue: false, required: false, submitOnChange: true
+            if (debugging) {
+                input "log#info", "bool", title: "Log info messages", defaultValue: true, required: false
+                input "log#trace", "bool", title: "Log trace messages", defaultValue: true, required: false
+                input "log#debug", "bool", title: "Log debug messages", defaultValue: true, required: false
+                input "log#warn", "bool", title: "Log warning messages", defaultValue: true, required: false
+                input "log#error", "bool", title: "Log error messages", defaultValue: true, required: false
+            }
+        }
+    }
+}
+
+def pageUninstall() {
+	dynamicPage(name: "pageUninstall", title: "Uninstall", install: false, uninstall: true) {
+		section() {
+        	paragraph "CAUTION: You are about to completely remove the SmartApp '${app.name}'. This action is irreversible. If you want to proceed, tap on the 'Remove' button below.",
+                required: true, state: null
+        }
+	}
+}
+
+
+//   ----------------------------
+//   ***   APP INSTALLATION   ***
+
+def installed() {
+	debug "installed with settings: ${settings}", "trace"
+	initialize()
+}
+
+def updated() {
+    debug "updated with settings ${settings}", "trace"
+	//unsubscribe()
+    initialize()
+}
+
+def uninstalled() {
+    state.debugLevel = 0
+    debug "application uninstalled", "trace"
+}
+
+def initialize() {
+    state.debugLevel = 0
+    debug "initializing", "trace", 1
+    debug "there are ${childApps.size()} child smartapps", "info"
+    childApps.each {child ->
+        debug "child app: ${child.label}", "info"
+    }
+    debug "initialization complete", "trace", -1
+}
+
+
+//   ------------------------
+//   ***   COMMON UTILS   ***
+
+def debug(message, lvl = null, shift = null, err = null) {
+	def debugging = settings.debugging
+	if (!debugging) {
+		return
+	}
+	lvl = lvl ?: "debug"
+	if (!settings["log#$lvl"]) {
+		return
+	}
+	
+    def maxLevel = 4
+	def level = state.debugLevel ?: 0
+	def levelDelta = 0
+	def prefix = "║"
+	def pad = "░"
+	
+    //shift is:
+	//	 0 - initialize level, level set to 1
+	//	 1 - start of routine, level up
+	//	-1 - end of routine, level down
+	//	 anything else - nothing happens
+	
+    switch (shift) {
+		case 0:
+			level = 0
+			prefix = ""
+			break
+		case 1:
+			level += 1
+			prefix = "╚"
+			pad = "═"
+			break
+		case -1:
+			levelDelta = -(level > 0 ? 1 : 0)
+			pad = "═"
+			prefix = "╔"
+			break
+	}
+
+	if (level > 0) {
+		prefix = prefix.padLeft(level, "║").padRight(maxLevel, pad)
+	}
+
+	level += levelDelta
+	state.debugLevel = level
+
+	if (debugging) {
+		prefix += " "
+	} else {
+		prefix = ""
+	}
+
+    if (lvl == "info") {
+        log.info ": :$prefix$message", err
+	} else if (lvl == "trace") {
+        log.trace "::$prefix$message", err
+	} else if (lvl == "warn") {
+		log.warn "::$prefix$message", err
+	} else if (lvl == "error") {
+		log.error "::$prefix$message", err
+	} else {
+		log.debug "$prefix$message", err
+	}
+}
+
+def testShare() { //TODO: remove once testing complete
+	return [key1: "value1", key2: "value2"]
+}
