112a12bfbe0e4c891bb7b44ce8257165b48586d9
Phil Maynard <astrowings@gmail.com>
2016-11-16 06:34:25 -0700

    v3.00

--- smartapps/astrowings/sunset-lights.src/sunset-lights.groovy
+++ smartapps/astrowings/sunset-lights.src/sunset-lights.groovy
@@ -15,10 +15,10 @@
  *
  *
  *	VERSION HISTORY										*/
- 	 private versionNum() {	return "version 2.24" }
-     private versionDate() { return "09-Nov-2016" }     /*
+ 	 private versionNum() {	return "version 3.00" }
+     private versionDate() { return "15-Nov-2016" }     /*
  *
- *    vx.xx (xx-Nov-2016) - code improvement: store images on GitHub, use getAppImg() to display app images
+ *    v3.00 (15-Nov-2016) - code improvement: store images on GitHub, use getAppImg() to display app images
  *                        - added option to disable icons
  *                        - added option to disable multi-level logging
  *                        - configured default values for app settings
