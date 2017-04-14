b487f54e59ab3f20760e6f2af2e1d46ff396aeb5
Phil Maynard <astrowings@gmail.com>
2017-04-14 15:46:17 -0600

    developing

---
+++ smartapps/astrowings/thermostat-notify.src/thermostat-notify.groovy
@@ -0,0 +1,404 @@
+/**
+ *  Thermostat Notify
+ *
+ *  Copyright © 2016 Phil Maynard
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0												*/
+ 	       private urlApache() { return "http://www.apache.org/licenses/LICENSE-2.0" }			/*
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *	VERSION HISTORY										*/
+ 	 private versionNum() {	return "version 0.10" }
+     private versionDate() { return "26-Mar-2017" }		/*
+ *
+ *    v1.00 (dd-mmm-2017) - initial release
+ *    v0.10 (26-Mar-2017) - developing
+ *
+*/
+definition(
+    name: "Thermostat Notify",
+    namespace: "astrowings",
+    author: "Phil Maynard",
+    description: "Send notifications based on selected thermostat events.",
+    category: "Convenience",
+    iconUrl: "https://raw.githubusercontent.com/astrowings/SmartThings/master/images/Cat-FunAndSocial.png",
+    iconX2Url: "https://raw.githubusercontent.com/astrowings/SmartThings/master/images/Cat-FunAndSocial@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/astrowings/SmartThings/master/images/Cat-FunAndSocial@3x.png"
+)
+
+
+//   ---------------------------
+//   ***   APP PREFERENCES   ***
+
+preferences {
+	page(name: "pageMain")
+    page(name: "pageSettings")
+    page(name: "pageLogOptions")
+    page(name: "pageAbout")
+    page(name: "pageUninstall")
+}
+
+
+//   --------------------------------
+//   ***   CONSTANTS DEFINITIONS  ***
+
+private		appImgPath()			{ return "https://raw.githubusercontent.com/astrowings/SmartThings/master/images/" }
+private		readmeLink()			{ return "https://github.com/astrowings/SmartThings/blob/master/smartapps/astrowings/thermostat-notify.src/readme.md" }
+
+
+//   -----------------------------
+//   ***   PAGES DEFINITIONS   ***
+
+def pageMain() {
+    dynamicPage(name: "pageMain", install: true, uninstall: false) {
+    	section(){
+        	paragraph "", title: "This SmartApp sends notifications based on selected thermostat events."
+        }
+        section() {
+            input "theThermostat", "capability.thermostatMode", title: "Which thermostat?", description: "Monitor mode changes on this thermostat", required: false, submitOnChange: true
+            //TODO: add option to monitor changes in temperature setpoint
+        }
+		section() {
+			if (theThermostat) {
+                input "sendPush", "bool", title: "Send push notification on selected events", defaultValue: true, required: false
+                input "sendSMS", "bool", title: "Send SMS notification on selected events", defaultValue: false, required: false, submitOnChange: true
+                if (sendSMS) {
+                	input "numberSMS", "phone", title: "Phone number", required: true
+                }
+                href "pageSettings", title: "App settings", description: "", image: getAppImg("configure_icon.png"), required: false
+            }
+            href "pageAbout", title: "About", description: "", image: getAppImg("info-icn.png"), required: false
+		}
+    }
+}
+
+def pageSettings() {
+	dynamicPage(name: "pageSettings", install: false, uninstall: false) {
+        section() {
+			label title: "Assign a name", defaultValue: "${app.name}", required: false
+            href "pageUninstall", title: "", description: "Uninstall this SmartApp", image: getAppImg("trash-circle-red-512.png"), state: null, required: true
+		}
+		section("Debugging Options", hideable: true, hidden: true) {
+            input "noAppIcons", "bool", title: "Disable App Icons", description: "Do not display icons in the configuration pages", image: getAppImg("disable_icon.png"), defaultValue: false, required: false, submitOnChange: true
+            href "pageLogOptions", title: "IDE Logging Options", description: "Adjust how logs are displayed in the SmartThings IDE", image: getAppImg("office8-icn.png"), required: true, state: "complete"
+        }
+    }
+}
+
+def pageAbout() {
+	dynamicPage(name: "pageAbout", title: "About this SmartApp", install: false, uninstall: false) { //with 'install: false', clicking 'Done' goes back to previous page
+		section() {
+        	href url: readmeLink(), title: app.name, description: "Copyright ©2017 Phil Maynard\n${versionNum()}", image: getAppImg("readme-icn.png")
+            href url: urlApache(), title: "License", description: "View Apache license", image: getAppImg("license-icn.png")
+		}
+    }
+}
+
+def pageLogOptions() {
+	dynamicPage(name: "pageLogOptions", title: "IDE Logging Options", install: false, uninstall: false) {
+        section() {
+	        input "debugging", "bool", title: "Enable debugging", description: "Display the logs in the IDE", defaultValue: false, required: false, submitOnChange: true 
+        }
+        if (debugging) {
+            section("Select log types to display") {
+                input "log#info", "bool", title: "Log info messages", defaultValue: true, required: false 
+                input "log#trace", "bool", title: "Log trace messages", defaultValue: true, required: false 
+                input "log#debug", "bool", title: "Log debug messages", defaultValue: true, required: false 
+                input "log#warn", "bool", title: "Log warning messages", defaultValue: true, required: false 
+                input "log#error", "bool", title: "Log error messages", defaultValue: true, required: false 
+			}
+            section() {
+                input "setMultiLevelLog", "bool", title: "Enable Multi-level Logging", defaultValue: true, required: false,
+                    description: "Multi-level logging prefixes log entries with special characters to visually " +
+                        "represent the hierarchy of events and facilitate the interpretation of logs in the IDE"
+            }
+        }
+    }
+}
+
+def pageUninstall() {
+	dynamicPage(name: "pageUninstall", title: "Uninstall", install: false, uninstall: true) {
+		section() {
+        	paragraph "CAUTION: You are about to completely remove the SmartApp '${app.name}'. This action is irreversible. If you want to proceed, tap on the 'Remove' button below.",
+                required: true, state: null
+        }
+	}
+}
+
+
+//   ---------------------------------
+//   ***   PAGES SUPPORT METHODS   ***
+
+
+
+//   ----------------------------
+//   ***   APP INSTALLATION   ***
+
+def installed() {
+	debug "installed with settings: ${settings}", "trace"
+	initialize()
+}
+
+def updated() {
+    debug "updated with settings ${settings}", "trace"
+	unsubscribe()
+    initialize()
+}
+
+def uninstalled() {
+    state.debugLevel = 0
+    debug "application uninstalled", "trace"
+}
+
+def initialize() {
+    state.debugLevel = 0
+    debug "initializing", "trace", 1
+    subscribeToEvents()
+    debug "initialization complete", "trace", -1
+}
+
+def subscribeToEvents() {
+    debug "subscribing to events", "trace", 1
+	subscribe(theThermostat, "nestThermostatMode", eventHandler)
+    subscribe(location, "mode", modeChangeHandler)
+    subscribe(location, "position", locationPositionChange) //update settings if hub location changes
+    debug "subscriptions complete", "trace", -1
+}
+
+
+//   --------------------------
+//   ***   EVENT HANDLERS   ***
+
+def eventHandler(evt) {
+	debug "${theThermostat.nestThermostatMode}"
+    /*if (sendPush) {
+    	sendNotification(evt.description)
+    }
+    if (sendSMS) {
+    	sendText(evt.description)
+    }*/
+}
+
+def locationPositionChange(evt) {
+    debug "locationPositionChange(${evt.descriptionText})", "warn"
+	initialize()
+}
+
+def modeChangeHandler(evt) {
+	debug "modeChangeHandler event: ${evt.descriptionText}", "trace"
+    //TODO: is there anything to do on mode change?
+    debug "modeChangeHandler complete", "trace"
+}
+
+
+//   -------------------
+//   ***   METHODS   ***
+
+def sendNotification(strMsg) {
+	debug "executing sendNotification()", "trace", 1
+    sendPush(strMsg)
+    debug "sendNotification() complete", "trace", -1
+}
+
+def sendText(strMsg) {
+    debug "executing sendText(msg: ${strMsg})", "trace", 1
+	sendSms(phone, strMsg)
+    debug "sendText() complete", "trace", -1
+}
+
+
+//   -------------------------
+//   ***   APP FUNCTIONS   ***
+
+
+//   ------------------------
+//   ***   COMMON UTILS   ***
+
+def convertToHMS(ms) {
+    int hours = Math.floor(ms/1000/60/60)
+    int minutes = Math.floor((ms/1000/60) - (hours * 60))
+    int seconds = Math.floor((ms/1000) - (hours * 60 * 60) - (minutes * 60))
+    double millisec = ms-(hours*60*60*1000)-(minutes*60*1000)-(seconds*1000)
+    int tenths = (millisec/100).round(0)
+    return "${hours}h${minutes}m${seconds}.${tenths}s"
+}
+
+def getAppImg(imgName, forceIcon = null) {
+	def imgPath = appImgPath()
+    return (!noAppIcons || forceIcon) ? "$imgPath/$imgName" : ""
+}
+
+def debug(message, lvl = null, shift = null, err = null) {
+	
+    def debugging = settings.debugging
+	if (!debugging) {
+		return
+	}
+    
+    lvl = lvl ?: "debug"
+	if (!settings["log#$lvl"]) {
+		return
+	}
+	
+    def multiEnable = (settings.setMultiLevelLog == false ? false : true) //set to true by default
+    def maxLevel = 4
+	def level = state.debugLevel ?: 0
+	def levelDelta = 0
+	def prefix = "║"
+	def pad = "░"
+	
+    //shift is:
+	//	 0 - initialize level, level set to 1
+	//	 1 - start of routine, level up
+	//	-1 - end of routine, level down
+	//	 anything else - nothing happens
+	
+    switch (shift) {
+		case 0:
+			level = 0
+			prefix = ""
+			break
+		case 1:
+			level += 1
+			prefix = "╚"
+			pad = "═"
+			break
+		case -1:
+			levelDelta = -(level > 0 ? 1 : 0)
+			pad = "═"
+			prefix = "╔"
+			break
+	}
+
+	if (level > 0) {
+		prefix = prefix.padLeft(level, "║").padRight(maxLevel, pad)
+	}
+
+	level += levelDelta
+	state.debugLevel = level
+
+	if (multiEnable) {
+		prefix += " "
+	} else {
+		prefix = ""
+	}
+
+    if (lvl == "info") {
+    	def leftPad = (multiEnable ? ": :" : "")
+        log.info "$leftPad$prefix$message", err
+	} else if (lvl == "trace") {
+    	def leftPad = (multiEnable ? "::" : "")
+        log.trace "$leftPad$prefix$message", err
+	} else if (lvl == "warn") {
+    	def leftPad = (multiEnable ? "::" : "")
+		log.warn "$leftPad$prefix$message", err
+	} else if (lvl == "error") {
+    	def leftPad = (multiEnable ? "::" : "")
+		log.error "$leftPad$prefix$message", err
+	} else {
+		log.debug "$prefix$message", err
+	}
+}
+
+
+/* from original code
+preferences {
+	section("Send this message (optional, sends standard status message if not specified)"){
+		input "messageText", "text", title: "Message Text", required: false
+	}
+	section("Via a push notification and/or an SMS message"){
+		input("recipients", "contact", title: "Send notifications to") {
+			input "phone", "phone", title: "Enter a phone number to get SMS", required: false
+			paragraph "If outside the US please make sure to enter the proper country code"
+			input "pushAndPhone", "enum", title: "Notify me via Push Notification", required: false, options: ["Yes", "No"]
+		}
+	}
+	section("Minimum time between messages (optional, defaults to every message)") {
+		input "frequency", "decimal", title: "Minutes", required: false
+	}
+}
+
+def eventHandler(evt) {
+	log.debug "Notify got evt ${evt}"
+	if (frequency) {
+		def lastTime = state[evt.deviceId]
+		if (lastTime == null || now() - lastTime >= frequency * 60000) {
+			sendMessage(evt)
+		}
+	}
+	else {
+		sendMessage(evt)
+	}
+}
+
+private sendMessage(evt) {
+	String msg = messageText
+	Map options = [:]
+
+	if (!messageText) {
+		msg = defaultText(evt)
+		options = [translatable: true, triggerEvent: evt]
+	}
+	log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
+
+	if (location.contactBookEnabled) {
+		sendNotificationToContacts(msg, recipients, options)
+	} else {
+		if (phone) {
+			options.phone = phone
+			if (pushAndPhone != 'No') {
+				log.debug 'Sending push and SMS'
+				options.method = 'both'
+			} else {
+				log.debug 'Sending SMS'
+				options.method = 'phone'
+			}
+		} else if (pushAndPhone != 'No') {
+			log.debug 'Sending push'
+			options.method = 'push'
+		} else {
+			log.debug 'Sending nothing'
+			options.method = 'none'
+		}
+		sendNotification(msg, options)
+	}
+	if (frequency) {
+		state[evt.deviceId] = now()
+	}
+}
+
+private defaultText(evt) {
+	if (evt.name == 'presence') {
+		if (evt.value == 'present') {
+			if (includeArticle) {
+				'{{ triggerEvent.linkText }} has arrived at the {{ location.name }}'
+			}
+			else {
+				'{{ triggerEvent.linkText }} has arrived at {{ location.name }}'
+			}
+		} else {
+			if (includeArticle) {
+				'{{ triggerEvent.linkText }} has left the {{ location.name }}'
+			}
+			else {
+				'{{ triggerEvent.linkText }} has left {{ location.name }}'
+			}
+		}
+	} else {
+		'{{ triggerEvent.descriptionText }}'
+	}
+}
+
+private getIncludeArticle() {
+	def name = location.name.toLowerCase()
+	def segs = name.split(" ")
+	!(["work","home"].contains(name) || (segs.size() > 1 && (["the","my","a","an"].contains(segs[0]) || segs[0].endsWith("'s"))))
+}
+*/
