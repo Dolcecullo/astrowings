58ba0fd6be0627103338d518fc0ab2a0fdacadc3
Philippe Maynard <astrowings@gmail.com>
2019-11-05 12:49:40 -0500

    update version number

--- smartapps/astrowings/thermostat-notify.src/thermostat-notify.groovy
+++ smartapps/astrowings/thermostat-notify.src/thermostat-notify.groovy
@@ -7,7 +7,7 @@
  *  in compliance with the License. You may obtain a copy of the License at:
  *
  *      http://www.apache.org/licenses/LICENSE-2.0												*/
- 	       def urlApache() { return "http://www.apache.org/licenses/LICENSE-2.0" }			/*
+ 	       def urlApache() { return "http://www.apache.org/licenses/LICENSE-2.0" }				/*
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
@@ -38,8 +38,8 @@ definition(
 //   --------------------------------
 //   ***   APP DATA  ***
 
-def		versionNum()			{ return "version 1.11" }
-def		versionDate()			{ return "08-Aug-2018" }     
+def		versionNum()			{ return "version 1.01" }
+def		versionDate()			{ return "09-Aug-2018" }     
 def		gitAppName()			{ return "thermostat-notify" }
 def		gitOwner()				{ return "astrowings" }
 def		gitRepo()				{ return "SmartThings" }
